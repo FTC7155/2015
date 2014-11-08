3a948de1d4a1f45c52bee6c1aa9e8acc3d9d9189
Jonathan Damcio <jj_damico@yahoo.com>
2014-11-08 15:52:47 -0800

    Fixed the tele op code for new base build
    CAMERON STOP REBUILDING THE BASE AND FOCUS ON GETTING MOTORS ON THE
    LIFTS!!!

--- Tele-Op/TeleOp.v1.c
+++ Tele-Op/TeleOp.v1.c
@@ -1,16 +1,14 @@
 #pragma config(Hubs,  S1, HTServo,  none,     none,     none)
-#pragma config(Hubs,  S2, HTMotor,  HTMotor,  none,     none)
+#pragma config(Hubs,  S2, HTMotor,  none,     none,     none)
 #pragma config(Hubs,  S4, HTMotor,  HTMotor,  none,     none)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
 #pragma config(Sensor, S2,     ,               sensorI2CMuxController)
 #pragma config(Sensor, S4,     ,               sensorI2CMuxController)
-#pragma config(Motor,  mtr_S2_C1_1,     leftWheel3,    tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S2_C1_2,     rightWheel3,   tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S2_C2_1,     leftLift,      tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S2_C2_2,     rightLift,     tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S4_C1_1,     leftWheel1,    tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S4_C1_2,     rightWheel1,   tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S4_C2_1,     rightWheel2,   tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S2_C1_1,     rightWheel1,   tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S2_C1_2,     rightWheel2,   tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C1_1,     rightLift,     tmotorTetrix, PIDControl, encoder)
+#pragma config(Motor,  mtr_S4_C1_2,     leftLift,      tmotorTetrix, PIDControl, encoder)
+#pragma config(Motor,  mtr_S4_C2_1,     leftWheel1,    tmotorTetrix, openLoop, reversed)
 #pragma config(Motor,  mtr_S4_C2_2,     leftWheel2,    tmotorTetrix, openLoop)
 #pragma config(Servo,  srvo_S1_C1_1,    servo1,               tServoNone)
 #pragma config(Servo,  srvo_S1_C1_2,    servo2,               tServoNone)
@@ -26,7 +24,7 @@
  Tele-Op Code
  Authors: Jonathan Damico, Nomi Ringoach
  Version 1
- Revised November 3, 2014
+ Since November 3, 2014
 */
 
 #include "JoystickDriver.c"
@@ -43,12 +41,10 @@ task main() //Main task for code
 		//Set left wheels
     motor[leftWheel1] = joystick.joy1_y1;
 		motor[leftWheel2] = joystick.joy1_y1;
-		motor[leftWheel3] = joystick.joy1_y1;
 
     //Set right wheels
 		motor[rightWheel1] = joystick.joy1_y2;
 		motor[rightWheel2] = joystick.joy1_y2;
-		motor[rightWheel3] = joystick.joy1_y2;
 
 	}
 }
